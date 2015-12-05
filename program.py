e303ac48a83dd199c226cc0b89b80534e0a3fae1
Bill McFarland <bemcfarland@gmail.com>
2015-12-05 18:51:23 +0000

    none

--- program.py
+++ program.py
@@ -1,12 +1,16 @@
 import urllib
 import xml.etree.ElementTree as ET
 
-while True:
+def main():
     url = 'http://python-data.dr-chuck.net/comments_42.xml'
     print 'Retrieving', url
     if len(url) < 1 : break
     uh = urllib.urlopen(url)
     data = uh.read()
     print 'Retrieved',len(data),'characters'
-    print data
+    #print data
     tree = ET.fromstring(data)
+    counts = tree.findall(´.//count´)
+    print counts
+    break
+main()
